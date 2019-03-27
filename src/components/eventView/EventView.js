40a22fc5a9992cfe48ba840ea61faafebbca376a
Bois Bakhodirov <bois.bb18@gmail.com>
2019-03-27 00:28:30 -0500

    YO-19: UI is set up

--- src/components/eventView/EventView.js
+++ src/components/eventView/EventView.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import {withRouter} from 'react-router-dom'
 import Icon from '@material-ui/core/Icon'
+import Button from '@material-ui/core/Button'
 import Header from '../header'
 
 class EventView extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      time: 0
+    }
     this.handleClose = this.handleClose.bind(this)
+    this.startTimer = this.startTimer.bind(this)
+  }
+
+  startTimer() {
+    this.setState({
+      time: this.state.time,
+      start: Date.now() - this.state.time
+    })
+    this.timer = setInterval(() => (
+      this.setState({
+        time: Date.now() - this.state.start
+      }),
+    1))
   }
 
   handleClose() {
-    this.props.history.push('/home')
+    console.log('Close Event --> ')
+    // this.props.history.push('/home')
   }
 
   render() {
     let {userInfo, event} = this.props
+    let tip = parseFloat(event.tipAmount).toFixed(2)
     console.log('Event --> ', event)
     return (
       <div className="EventView">
@@ -34,6 +53,18 @@ class EventView extends React.Component {
         </div>
         <div className="EventView--icon" />
        </div>
+       <div className="EventView--address">{event.address}</div>
+       <div className="EventView__tip-container">
+        Minimum tip required for request:
+        <p>${tip}</p>
+       </div>
+       <div className="EventView__request-container">
+        2 Song Request Completed
+        <p>$3.00 Earned</p>
+       </div>
+       <div className="EventView--end-action">
+        <Button variant="contained" color="primary" classes={{root: 'EventView__end'}} onClick={this.handleClose}>End</Button>
+      </div>
       </div>
     )
   }
