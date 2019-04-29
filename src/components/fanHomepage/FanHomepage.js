4f576b56ccba0edb25f659542bd672e35abbb923
Bois Bakhodirov <bois.bb18@gmail.com>
2019-04-28 22:27:23 -0500

    YO-43: temporary save

---
+++ src/components/fanHomepage/FanHomepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class FanHomepage extends React.Component {
+    render() {
+        return (
+            <div className="FanHomepage">
+                Hello Fanhomepage
+            </div>
+        )
+    }
+}
+
+export default FanHomepage
