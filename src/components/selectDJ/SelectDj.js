7f2b299e50352e045d4eef6726e844d6d8f6e7ca
Bois Bakhodirov <bois.bb18@gmail.com>
2019-04-30 20:04:23 -0500

    YO-45: Skeleton is set up

--- src/components/selectDJ/SelectDj.js
+++ src/components/selectDJ/SelectDj.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {withRouter} from 'react-router-dom'
+import Icon from '@material-ui/core/Icon'
 import Header from '../header'
 
 
@@ -7,6 +8,11 @@ class SelectDj extends React.Component {
   constructor(props) {
     super(props)
     this.openProfile = this.openProfile.bind(this)
+    this.close = this.close.bind(this)
+  }
+
+  close() {
+    this.props.onGoBack()
   }
 
   openProfile() {
@@ -18,7 +24,29 @@ class SelectDj extends React.Component {
     return(
       <div className="SelectDj">
         <Header imageUrl={userInfo.imageUrl} iconClick={this.openProfile} isActive={false} />
-        Hello Select DJ
+        <div className="SelectDj__subheader">
+          <Icon onClick ={this.close}>close</Icon>
+          <div className="SelectDj--subtitle">Venues/DJs</div>
+          <div className="SelectDj--onlines">3 online</div>
+       </div>
+       <div className="SelectDj__container">
+          <div className="SelectDj__dj-wrapper">
+            <div className="SelectDj__icon-container"> 
+              <div className="SelectDj--headset">
+                  <Icon classes={{root: `SelectDj--headset-icon`}}>headset</Icon>
+              </div>
+              <div className="SelectDj--icon" />
+            </div>
+            <div className="SelectDj--info-container">
+              <div className="SelectDj--general">@Nvd <span className="SelectDj--place">The Mid</span></div>
+              <div className="SelectDj--time">11 PM</div>
+              <div className="SelectDj--tip">Request minimum $2.00</div>
+            </div>
+          </div>
+          <div className="SelectDj--forward">
+            <Icon classes={{root: `SelectDj--arrow`}}>close</Icon>
+          </div>
+       </div>
       </div>
     )
   }
