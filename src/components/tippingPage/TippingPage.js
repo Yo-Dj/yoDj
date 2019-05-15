b291c0aa5634c7430c32cfe5392aa16327cbd371
Bois Bakhodirov <bois.bb18@gmail.com>
2019-05-15 00:05:09 -0500

    YO-50: UI set up

--- src/components/tippingPage/TippingPage.js
+++ src/components/tippingPage/TippingPage.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import Icon from '@material-ui/core/Icon'
 import Button from '@material-ui/core/Button'
+import Input from '@material-ui/core/Input'
+import TextField from '@material-ui/core/TextField'
 import Header from '../header'
 
 class TippingPage extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            tipText: '',
+            searchText: ''
+        }
+        this.tipChange = this.tipChange.bind(this)
         this.leaveEvent = this.leaveEvent.bind(this)
+        this.search = this.search.bind(this)
     }
 
     leaveEvent() {
         console.log('Leave the event')
+    }
+
+    search(e) {
+        this.setState({
+            searchText: e.target.value
+        })
+    }
+
+    tipChange(e) {
+        this.setState({
+            tipText: e.target.value
+        })
     }
 
     render() {
@@ -22,7 +42,7 @@ class TippingPage extends React.Component {
             }
             return acc
         }, {})
-
+        let tip = parseFloat(fanEvent.tipAmount).toFixed(2)
         console.log('Dj ---> ', eventDj)
         return (
             <div className="TippingPage">
@@ -42,6 +62,45 @@ class TippingPage extends React.Component {
                             Leave Event
                         </Button>
                     </div>
+                </div>
+                <div className="TippingPage__tip-container">
+                    <div className="TippingPage--amount">
+                        <div className="TippingPage--subtitle">Tip<span className="TippingPage--subtitle-icon">></span></div>
+                        <div className="TippingPage--input-container">
+                            $
+                            <div className="TippingPage--input">
+                                <TextField
+                                    value={this.state.tipText}
+                                    margin="normal"
+                                    onChange={this.tipChange}
+                                    classes={{root: "TippingPage--tip-text"}}
+                                    InputProps={{style: {textAlign: 'start', margin: '0 10px'}}}
+                                />
+                            </div>
+                            <div className="TippingPage--user-icon">
+                                {/* <Icon classes={{root: `TippingPage--sample-user`}}>account-box</Icon> */}
+                                <svg className="TippingPage--sample-user" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26"><path d="M15 2H3c-.55 0-1 .45-1 1v12c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zM9 4.75c1.24 0 2.25 1.01 2.25 2.25S10.24 9.25 9 9.25 6.75 8.24 6.75 7 7.76 4.75 9 4.75zM13.5 14h-9v-.75c0-1.5 3-2.25 4.5-2.25s4.5.75 4.5 2.25V14z"/></svg>								
+                            </div>
+                        </div>
+                    </div>
+                    <div className="TippingPage--minimum">
+                        <span className="TippingPage--tip-amount">Song Request</span>
+                        minimum ${tip}
+                    </div>
+                    <div className="TippingPage--search-container">
+                        <div className="TippingPage--music-icon" />
+                        <div className="TippingPage--search-input">
+                                <TextField
+                                    value={this.state.searchText}
+                                    margin="normal"
+                                    placeholder="Search song, artist, album"
+                                    onChange={this.search}
+                                    classes={{root: "TippingPage--search-text"}}
+                                    InputProps={{style: {textAlign: 'start', margin: '0 20px'}}}
+                                />
+                            </div>
+                    </div>
+
                 </div>
             </div>
         )
